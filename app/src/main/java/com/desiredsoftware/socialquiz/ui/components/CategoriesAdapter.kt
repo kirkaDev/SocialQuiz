9f66db0b0fdc75b097b8f561aa35d38ea152c3d2
Kirill <kirkadev@gmail.com>
2021-11-22 01:50:05 +0500

    Shimmer for category card implemented

--- app/src/main/java/com/desiredsoftware/socialquiz/ui/components/CategoriesAdapter.kt
+++ app/src/main/java/com/desiredsoftware/socialquiz/ui/components/CategoriesAdapter.kt
@@ -8,46 +8,57 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.desiredsoftware.socialquiz.R
 import com.desiredsoftware.socialquiz.model.category.Category
+import com.facebook.shimmer.ShimmerFrameLayout
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
-class CategoriesAdapter (private val categoriesList: ArrayList<Category>, private val onClickCategoryListener: OnClickCategoryListener):
-        RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder>(){
 
+class CategoriesAdapter(
+    private val categoriesList: ArrayList<Category>,
+    private val onClickCategoryListener: OnClickCategoryListener
+) :
+    RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder>() {
 
     override fun getItemCount(): Int {
         return categoriesList.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
         return CategoriesViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
-
         Picasso.get().load(categoriesList[position].imageResource)
-                .resize(120, 120)
-                .centerCrop()
-                .into(holder.imageCategory)
+            .resize(120, 120)
+            .centerCrop()
+            .into(holder.imageCategory, object : Callback {
+                override fun onSuccess() {
+                    holder.shimmer?.setShimmer(null)
+                }
+
+                override fun onError(e: Exception?) {
+                    // TODO: Set placeholder here
+                    holder.shimmer?.setShimmer(null)
+                }
+            })
 
         holder.textViewCategoryName?.text = categoriesList[position].categoryName
-
         holder.imageCategory?.setOnClickListener {
             onClickCategoryListener.onClicked(categoriesList[position].id)
         }
-
     }
 
-
-    class CategoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    {
+    class CategoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imageCategory: ImageView? = null
         var textViewCategoryName: TextView? = null
+        var shimmer: ShimmerFrameLayout? = null
 
         init {
             imageCategory = itemView.findViewById(R.id.imageCategory)
             textViewCategoryName = itemView.findViewById(R.id.textViewCategoryName)
+            shimmer = itemView.findViewById(R.id.shimmer)
         }
     }
-
 }
